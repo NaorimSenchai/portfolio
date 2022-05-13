dc5e587cda78fa5abde46c72c4c49d6be888c58b
NaorimSenchai <romainchenais35@gmail.com>
2022-05-13 16:35:01 +0200

    Started responsive work

--- src/server.js
+++ src/server.js
@@ -3,12 +3,12 @@ const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 const ejs = require("ejs");
-var bodyParser = require('body-parser');
+const knex = require("./models/knexClient");
+const bodyParser = require('body-parser');
 require("dotenv").config();
 
-
 // EXTERNAL SCRIPTS IMPORTS
-const mail = require("../routes/mail");
+const mail = require("./routes/mail");
 
 // SERVER SETTINGS
 app.set("views engine", "ejs");
@@ -19,21 +19,25 @@ app.use(cookieParser());
 
 // --- ROUTES SETTINGS
 // app.use("/admin", mail);
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
 	res.render("pages/index.ejs");
 });
-app.get("/projects", (req, res) => {
-	res.render("pages/projects.ejs");
+app.get("/projects", async (req, res) => {
+	try {
+		const data = await knex('project').select();
+		console.log(data);
+		res.render("pages/projects.ejs", {data});
+	} catch (err) {
+		res.send(err.message);
+	};
+	
 });
-app.get("/sharables", (req, res) => {
+app.get("/sharables", async (req, res) => {
 	res.render("pages/sharables.ejs");
 });
-app.get("/history", (req, res) => {
+app.get("/history", async (req, res) => {
 	res.render("pages/history.ejs");
 });
-
-// DATABASE LAUNCHING
-
 
 // SERVER LAUNCHING
 app.listen(process.env.PORT, () => console.log(`Server started on http://www.localhost:${process.env.PORT}`));
